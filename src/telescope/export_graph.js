02ee43804caea74a6a83e8d8863515e1c0293872
Greaby <hello@greaby.co>
2021-09-20 16:07:57 +0200

    Optimization of the JSON file size

--- src/telescope/export_graph.js
+++ src/telescope/export_graph.js
@@ -4,15 +4,18 @@ const random = require("graphology-layout/random");
 module.exports = (graph) => {
     random.assign(graph);
 
-    graph.nodes().forEach((node) => {
-        graph.removeNodeAttribute(node, "pagerank");
-    });
-
     forceAtlas2.assign(graph, {
         iterations: 1000,
         settings: {
             gravity: 0.8,
         },
+    });
+
+    // Optimization of the JSON file size
+    graph.nodes().forEach((node) => {
+        graph.removeNodeAttribute(node, "pagerank");
+        graph.updateNodeAttribute(node, "x", (x) => Math.round(x));
+        graph.updateNodeAttribute(node, "y", (y) => Math.round(y));
     });
 
     return JSON.stringify(graph.export());
